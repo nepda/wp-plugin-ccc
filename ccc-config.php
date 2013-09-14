d18ff633740e774ede7a4e9ecf5c5a73873f632f
Nepomuk Fraedrich <info@nepda.eu>
2013-09-14 13:24:27 +0200

    Initial commit

---
+++ ccc-config.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * PHP Version >= 5.4
+ *
+ * @category  WordPress
+ * @package   WordPress_CommentCompetitionConfirm
+ * @author    Nepomuk Fraedrich <info@nepda.eu>
+ */
+
+// TODO define this via plugin config
+
+/**
+ * The value of CCC_CUSTOM_KEY is needed as custom key in the post
+ *
+ * If CCC_CUSTOM_KEY as custom key in the post has any value, the checkbox will appear in the comment form,
+ * if the value is not set, the checkbox will not be processed. Tell this value your publisher!
+ */
+define('CCC_CUSTOM_KEY',      'gewinnspiel');
+
+/**
+ * The CCC_COLUMN_NAME will be used to name the value inside the DB.
+ *
+ * Your input fields will also have this id and name (for designers)
+ */
+define('CCC_COLUMN_NAME',     'competition_rules');
+
+/**
+ * This CCC_CHECKBOX_TEXT will appear as label for your checkbox. You can place a link inside to your competition
+ * conditions
+ */
+define('CCC_CHECKBOX_TEXT',   'Wenn Sie am Gewinnspiel teilnehmen m&ouml;chten, lesen Sie bitte
+                               <a href="/gewinnspiel-teilnahmebedingungen">die Gewinnspiel Teilnahmebedingungen</a> und
+                               stimmen ihnen zu. Ansonsten kann Ihre Antwort leider nicht ausgewertet werden.');
+
+/**
+ * CCC_CHECKED_VALUE will be put in the DB and shown in your admin dashboard if the user has checked the checkbox
+ */
+define('CCC_CHECKED_VALUE',   'akzeptiert: ja');
+
+/**
+ * CCC_UNCHECKED_VALUE will be put in the DB and shown in your admin dashboard if the user has NOT checked the checkbox
+ */
+define('CCC_UNCHECKED_VALUE', 'akzeptiert: nein');
+
+/**
+ * The value of CCC_ADMIN_TEXT will be shown as column head in your admin dashboard
+ */
+define('CCC_ADMIN_TEXT',      'Gewinnspiel akzeptiert?');
---
+++ plugin.php
@@ -0,0 +1,91 @@
+<?php
+/*
+  Plugin Name: Gewinnspielteilnahme Bedingungen (CCC (Comment Competition Confirm))
+  Plugin URI: http://github.com/nepda/wp-plugin-comment-competition-confirm
+  Description: Fuegt fuer gewisse Beitraege eine zusaetzliche Checkbox an das Kommentarformular um beispielsweise Gewinnspielteilnamebedingungen per Kommentar zu akzeptieren.
+  Version: 1.0
+  Author: Nepomuk Fraedrich
+  Author URI: http://nepda.eu
+ */
+
+/**
+ * PHP Version >= 5.4
+ *
+ * @category  WordPress
+ * @package   WordPress_CommentCompetitionConfirm
+ * @author    Nepomuk Fraedrich <info@nepda.eu>
+ */
+
+require_once 'ccc-config.php';
+
+// add the checkbox field to the comment form
+function ccc_add_comment_fields($fields) {
+
+    if( get_post_custom_values(CCC_CUSTOM_KEY) ) {
+        $fields['competition_rules'] = '<p class="comment-form-public">
+                   <input id="' . CCC_COLUMN_NAME . '"
+                    name="' . CCC_COLUMN_NAME . '" type="checkbox" />
+                   <label for="' . CCC_COLUMN_NAME . '">
+                   ' . CCC_CHECKBOX_TEXT . '
+                   </label></p>';
+    }
+    return $fields;
+}
+add_filter('comment_form_default_fields', 'ccc_add_comment_fields');
+
+
+// save the checkbox value into comment meta table
+function ccc_save_comment_meta_checkbox ( $post_id ) {
+    $save_meta_checkbox = $_POST['competition_rules'];
+
+
+    if ( $save_meta_checkbox == 'on' ) {
+        $value = CCC_CHECKED_VALUE;
+
+    } else {
+        $value = CCC_UNCHECKED_VALUE;
+
+    }
+    add_comment_meta( $post_id, CCC_COLUMN_NAME, $value, true );
+}
+add_action( 'comment_post', 'ccc_save_comment_meta_checkbox', 1);
+
+
+// add the column to the comment table view (at the end of the columns)
+// called by ccc_editcomments_load (add_filter())
+function ccc_editcomments_add_columns( $columns ) {
+
+    $columns['competition_rules'] = CCC_ADMIN_TEXT;
+    return $columns;
+}
+
+
+// add filter for admin comment edit view
+function ccc_editcomments_load()
+{
+    $screen = get_current_screen();
+
+    add_filter("manage_{$screen->id}_columns", 'ccc_editcomments_add_columns');
+}
+add_action('load-edit-comments.php', 'ccc_editcomments_load');
+
+
+// finally output the value
+function ccc_competition_column_cb($col, $comment_id)
+{
+    // you could expand the switch to take care of other custom columns
+    switch($col)
+    {
+        case CCC_COLUMN_NAME:
+            if($t = get_comment_meta($comment_id, CCC_COLUMN_NAME, true))
+            {
+                echo esc_html($t);
+            }
+            else
+            {
+                echo esc_html('');
+            }
+            break;
+    }
+}
+add_action('manage_comments_custom_column', 'ccc_competition_column_cb', 10, 2);
